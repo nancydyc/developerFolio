ea09194e517ea5364d7cb0975a2229964af6e1ce
Yichen <msdaiyichen@gmail.com>
2020-07-14 16:43:11 -0700

    Deleted missing sections from header menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -27,9 +27,6 @@ function Header() {
             </li>
           }
           <li>
-            <a href="#opensource">Open Source</a>
-          </li>
-          <li>
             <a href="#projects">Projects</a>
           </li>
           <li>
@@ -37,9 +34,6 @@ function Header() {
           </li>
           <li>
             <a href="#blogs">Blogs</a>
-          </li>
-          <li>
-            <a href="#talks">Talks</a>
           </li>
           <li>
             <a href="#contact">Contact Me</a>
