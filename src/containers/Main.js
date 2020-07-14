74470f69df6108c14bcd0f547979bdf27b5b08b0
Yichen <msdaiyichen@gmail.com>
2020-07-14 16:30:02 -0700

    Deleted talk section

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -10,7 +10,6 @@ import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
 import Contact from "./contact/Contact";
 import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
 import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
@@ -29,7 +28,6 @@ export default class Main extends Component {
         <StartupProject />
         <Achievement />
         <Blogs />
-        <Talks />
         <Twitter />
         <Podcast />
         <Profile />
